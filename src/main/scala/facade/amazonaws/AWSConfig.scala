833767e29646e9aae8a88ec628f92e66a6c73aa4
exoego <mogami@exoego.net>
2018-12-26 18:58:35 +0900

    Make AWSConfig mutable so global config can be updated directly

--- src/main/scala/facade/amazonaws/AWSConfig.scala
+++ src/main/scala/facade/amazonaws/AWSConfig.scala
@@ -1,37 +1,36 @@
 package facade.amazonaws
 
 import scala.scalajs.js
-import scala.scalajs.js.|
 
 class AWSConfig(
-  computeChecksums: js.UndefOr[Boolean] = js.undefined,
-  convertResponseTypes: js.UndefOr[Boolean] = js.undefined,
-  correctClockSkew: js.UndefOr[Boolean] = js.undefined,
-  customUserAgent: js.UndefOr[String] = js.undefined,
-  credentials: js.UndefOr[AWSCredentials] = js.undefined,
-  credentialProvider: js.UndefOr[js.Any] = js.undefined,
-  accessKeyId: js.UndefOr[String] = js.undefined,
-  secretAccessKey: js.UndefOr[String] = js.undefined,
-  sessionToken: js.UndefOr[String] = js.undefined,
-  httpOptions: js.UndefOr[HttpOptions] = js.undefined,
-  logger: js.UndefOr[Logger] = js.undefined,
-  maxRetries: js.UndefOr[Int] = js.undefined,
-  maxRedirects: js.UndefOr[Int] = js.undefined,
-  paramValidation: js.UndefOr[ParamValidation | Boolean] = js.undefined,
-  region: js.UndefOr[String] = js.undefined,
-  retryDelayOptions: js.UndefOr[RetryDelayOptions] = js.undefined,
-  s3BucketEndpoint: js.UndefOr[Boolean] = js.undefined,
-  s3DisableBodySigning: js.UndefOr[Boolean] = js.undefined,
-  s3ForcePathStyle: js.UndefOr[Boolean] = js.undefined,
-  signatureCache: js.UndefOr[Boolean] = js.undefined,
-  signatureVersion: js.UndefOr[String] = js.undefined,
-  sslEnabled: js.UndefOr[Boolean] = js.undefined,
-  systemClockOffset: js.UndefOr[Double] = js.undefined,
-  useAccelerateEndpoint: js.UndefOr[Boolean] = js.undefined,
-  dynamoDbCrc32: js.UndefOr[Boolean] = js.undefined,
-  endpointDiscoveryEnabled: js.UndefOr[Boolean] = js.undefined,
-  endpointCacheSize: js.UndefOr[Double] = js.undefined,
-  hostPrefixEnabled: js.UndefOr[Boolean] = js.undefined) extends js.Object
+  var computeChecksums: js.UndefOr[Boolean] = js.undefined,
+  var convertResponseTypes: js.UndefOr[Boolean] = js.undefined,
+  var correctClockSkew: js.UndefOr[Boolean] = js.undefined,
+  var customUserAgent: js.UndefOr[String] = js.undefined,
+  var credentials: js.UndefOr[AWSCredentials] = js.undefined,
+  var credentialProvider: js.UndefOr[js.Any] = js.undefined,
+  var accessKeyId: js.UndefOr[String] = js.undefined,
+  var secretAccessKey: js.UndefOr[String] = js.undefined,
+  var sessionToken: js.UndefOr[String] = js.undefined,
+  var httpOptions: HttpOptions = HttpOptions(),
+  var logger: js.UndefOr[Logger] = js.undefined,
+  var maxRetries: js.UndefOr[Int] = js.undefined,
+  var maxRedirects: js.UndefOr[Int] = js.undefined,
+  var paramValidation: ParamValidation = ParamValidation(),
+  var region: js.UndefOr[String] = js.undefined,
+  var retryDelayOptions: RetryDelayOptions = RetryDelayOptions(),
+  var s3BucketEndpoint: js.UndefOr[Boolean] = js.undefined,
+  var s3DisableBodySigning: js.UndefOr[Boolean] = js.undefined,
+  var s3ForcePathStyle: js.UndefOr[Boolean] = js.undefined,
+  var signatureCache: js.UndefOr[Boolean] = js.undefined,
+  var signatureVersion: js.UndefOr[String] = js.undefined,
+  var sslEnabled: js.UndefOr[Boolean] = js.undefined,
+  var systemClockOffset: js.UndefOr[Double] = js.undefined,
+  var useAccelerateEndpoint: js.UndefOr[Boolean] = js.undefined,
+  var dynamoDbCrc32: js.UndefOr[Boolean] = js.undefined,
+  var endpointDiscoveryEnabled: js.UndefOr[Boolean] = js.undefined,
+  var endpointCacheSize: js.UndefOr[Double] = js.undefined,
+  var hostPrefixEnabled: js.UndefOr[Boolean] = js.undefined) extends js.Object
 
 object AWSConfig {
   def apply(
@@ -44,13 +43,13 @@ object AWSConfig {
     accessKeyId: js.UndefOr[String] = js.undefined,
     secretAccessKey: js.UndefOr[String] = js.undefined,
     sessionToken: js.UndefOr[String] = js.undefined,
-    httpOptions: js.UndefOr[HttpOptions] = js.undefined,
+    httpOptions: HttpOptions = HttpOptions(),
     logger: js.UndefOr[Logger] = js.undefined,
     maxRetries: js.UndefOr[Int] = js.undefined,
     maxRedirects: js.UndefOr[Int] = js.undefined,
-    paramValidation: js.UndefOr[ParamValidation | Boolean] = js.undefined,
+    paramValidation: ParamValidation = ParamValidation(),
     region: js.UndefOr[String] = js.undefined,
-    retryDelayOptions: js.UndefOr[RetryDelayOptions] = js.undefined,
+    retryDelayOptions: RetryDelayOptions = RetryDelayOptions(),
     s3BucketEndpoint: js.UndefOr[Boolean] = js.undefined,
     s3DisableBodySigning: js.UndefOr[Boolean] = js.undefined,
     s3ForcePathStyle: js.UndefOr[Boolean] = js.undefined,
@@ -68,12 +67,12 @@ object AWSConfig {
 }
 
 class HttpOptions(
-  proxy: js.UndefOr[String] = js.undefined,
-  agent: js.UndefOr[js.Any] = js.undefined,
-  connectTimeout: js.UndefOr[Int] = js.undefined,
-  timeout: js.UndefOr[Int] = js.undefined,
-  xhrAsync: js.UndefOr[Boolean] = js.undefined,
-  xhrWithCredentials: js.UndefOr[Boolean] = js.undefined) extends js.Object
+  var proxy: js.UndefOr[String] = js.undefined,
+  var agent: js.UndefOr[js.Any] = js.undefined,
+  var connectTimeout: js.UndefOr[Int] = js.undefined,
+  var timeout: js.UndefOr[Int] = js.undefined,
+  var xhrAsync: js.UndefOr[Boolean] = js.undefined,
+  var xhrWithCredentials: js.UndefOr[Boolean] = js.undefined) extends js.Object
 
 object HttpOptions {
   def apply(
@@ -92,10 +91,10 @@ trait Logger {
 }
 
 class ParamValidation(
-  min: js.UndefOr[Boolean] = js.undefined,
-  max: js.UndefOr[Boolean] = js.undefined,
-  pattern: js.UndefOr[Boolean] = js.undefined,
-  enum: js.UndefOr[Boolean] = js.undefined) extends js.Object
+  var min: js.UndefOr[Boolean] = js.undefined,
+  var max: js.UndefOr[Boolean] = js.undefined,
+  var pattern: js.UndefOr[Boolean] = js.undefined,
+  var enum: js.UndefOr[Boolean] = js.undefined) extends js.Object
 
 object ParamValidation {
   def apply(
@@ -106,11 +105,11 @@ object ParamValidation {
 }
 
 class RetryDelayOptions(
-  base: js.UndefOr[Double],
-  customBackoff: js.UndefOr[Double => Double]) extends js.Object
+  var base: js.UndefOr[Double] = js.undefined,
+  var customBackoff: js.UndefOr[Double => Double] = js.undefined) extends js.Object
 
 object RetryDelayOptions {
   def apply(
-    base: js.UndefOr[Double],
-    customBackoff: js.UndefOr[Double => Double]): RetryDelayOptions = new RetryDelayOptions(base, customBackoff)
+    base: js.UndefOr[Double] = js.undefined,
+    customBackoff: js.UndefOr[Double => Double] = js.undefined): RetryDelayOptions = new RetryDelayOptions(base, customBackoff)
 }
