9e7c42caec79563213bb7fa00d949f9a1677103b
exoego <ytatsuno.jp@gmail.com>
2020-08-05 10:13:28 +0900

    Add S3.deleteObjectsByPrefixFuture extension method

--- services/s3/src/main/scala/facade/amazonaws/services/S3.scala
+++ services/s3/src/main/scala/facade/amazonaws/services/S3.scala
@@ -292,7 +292,7 @@ package object s3 {
     /**
       * Get a pre-signed URL for a given operation name.
       *
-      * @see http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getSignedUrl-property
+      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getSignedUrl-property
       * @param operation the name of the operation to call. E.g. `getObject`
       * @param params    Parameters to pass to the operation. See the given operation for the expected operation parameters. In addition, you can also pass the "Expires" parameter to inform S3 how long the URL should work for.
       * @param expires   The number of seconds to expire the pre-signed URL operation in. Defaults to 900 seconds (15 minutes).
@@ -310,6 +310,48 @@ package object s3 {
         .getSignedUrlPromise(operation, paramsWithExpires)
         .asInstanceOf[js.Promise[String]]
         .toFuture
+    }
+
+    /**
+      * Delete objects whose key name start with the given prefix.
+      *
+      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listObjectsV2-property
+      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteObjects-property
+      * @param bucket The name of the bucket
+      * @param prefix An object key name prefix. To include all objects in bucket, specify an empty string.
+      * @return Future of unit (no value)
+      */
+    def deleteObjectsByPrefixFuture(bucket: String, prefix: String)(implicit executor: scala.concurrent.ExecutionContext): Future[Unit] = {
+      def deleteObjects(objects: ObjectIdentifierList): Future[Unit] = {
+        if (objects.nonEmpty) {
+          service.deleteObjectsFuture(
+            DeleteObjectsRequest(
+              Bucket = bucket,
+              Delete = Delete(Objects = objects)
+            )
+          ).map(_ => ())
+        } else {
+          Future.unit
+        }
+      }
+      def loop(): Future[Unit] = {
+        for {
+          listObjectsResult <- service.listObjectsV2Future(
+            ListObjectsV2Request(
+              Bucket = bucket,
+              Prefix = prefix,
+              MaxKeys = 1000 // 1000 is upper-limit of listObjectsV2 and deleteObjects
+            )
+          )
+          objectsToDelete = listObjectsResult.Contents.get.map(x => ObjectIdentifier(Key = x.Key.get))
+          _ <- deleteObjects(objectsToDelete)
+          _ <- listObjectsResult.NextContinuationToken.fold(Future.unit) { _ =>
+            // The value of NextContinuationToken is ignored here, since it was revoked by object deletion
+            loop()
+          }
+        } yield {}
+      }
+      loop()
     }
 
   }
