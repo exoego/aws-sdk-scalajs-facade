33277fed79e6fe8b7c374c8a2ff4d264373b5f18
exoego <ytatsuno.jp@gmail.com>
2020-08-06 22:03:41 +0900

    Add DynamoDBConverter
    See https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/Converter.html

--- services/dynamodb/src/main/scala/facade/amazonaws/services/DynamoDB.scala
+++ services/dynamodb/src/main/scala/facade/amazonaws/services/DynamoDB.scala
@@ -184,6 +184,33 @@ package object dynamodb {
     @inline def updateTimeToLiveFuture(params: UpdateTimeToLiveInput): Future[UpdateTimeToLiveOutput] = service.updateTimeToLive(params).promise().toFuture
 
   }
+
+  @js.native
+  @JSImport("aws-sdk", "DynamoDB.Converter", "AWS.DynamoDB")
+  object DynamoDBConverter extends js.Object {
+    def input(data: js.Any, options: DynamoDBConverterOptions = js.native): AttributeValue = js.native
+    def output(data: AttributeValue, options: DynamoDBConverterOptions = js.native): js.Any = js.native
+
+    def marshall(data: js.Object, options: DynamoDBConverterOptions = js.native): AttributeMap = js.native
+    def unmarshall(data: AttributeMap, options: DynamoDBConverterOptions = js.native): js.Object = js.native
+  }
+
+  trait DynamoDBConverterOptions extends js.Object {
+    var convertEmptyValues: js.UndefOr[Boolean] = js.undefined
+    var wrapNumbers: js.UndefOr[Boolean] = js.undefined
+  }
+
+  object DynamoDBConverterOptions {
+    @inline def apply(
+        convertEmptyValues: js.UndefOr[Boolean] = js.undefined,
+        wrapNumbers: js.UndefOr[Boolean] = js.undefined
+    ): DynamoDBConverterOptions = {
+      val __obj = js.Dynamic.literal()
+      convertEmptyValues.foreach(__v => __obj.updateDynamic("convertEmptyValues")(__v.asInstanceOf[js.Any]))
+      wrapNumbers.foreach(__v => __obj.updateDynamic("wrapNumbers")(__v.asInstanceOf[js.Any]))
+      __obj.asInstanceOf[DynamoDBConverterOptions]
+    }
+  }
 }
 
 package dynamodb {
