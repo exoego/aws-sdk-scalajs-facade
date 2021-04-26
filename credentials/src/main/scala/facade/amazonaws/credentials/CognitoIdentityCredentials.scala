e4cb1a228e37e101e0f7f94dadceb9a1809aa9dd
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-04-26 09:46:42 +0900

    Remove LoginId due to typing issue

--- credentials/src/main/scala/facade/amazonaws/credentials/CognitoIdentityCredentials.scala
+++ credentials/src/main/scala/facade/amazonaws/credentials/CognitoIdentityCredentials.scala
@@ -8,11 +8,21 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 import scala.scalajs.js.|
 
+// TODO: support { LoginId: js.UndefOr[String] } in options
 @js.native
 @JSImport("aws-sdk/lib/node_loader", "CognitoIdentityCredentials", "AWS.CognitoIdentityCredentials")
 class CognitoIdentityCredentials() extends AWSCredentials {
-  def this(options: CognitoIdentityOptions) = this()
-  def this(options: CognitoIdentityOptions, clientConfig: AWSConfig) = this()
+  def this(options: cognitoidentity.GetIdInput) = this()
+  def this(options: cognitoidentity.GetIdInput, clientConfig: AWSConfig) = this()
+
+  def this(options: cognitoidentity.GetCredentialsForIdentityInput) = this()
+  def this(options: cognitoidentity.GetCredentialsForIdentityInput, clientConfig: AWSConfig) = this()
+
+  def this(options: cognitoidentity.GetOpenIdTokenInput) = this()
+  def this(options: cognitoidentity.GetOpenIdTokenInput, clientConfig: AWSConfig) = this()
+
+  def this(options: sts.AssumeRoleWithWebIdentityRequest) = this()
+  def this(options: sts.AssumeRoleWithWebIdentityRequest, clientConfig: AWSConfig) = this()
 
   def clearCacheId(): Unit = js.native
 
