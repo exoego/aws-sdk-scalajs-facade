f7bf40ab271c23e6e0824e7558c7fdca119bb3cf
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-12-28 07:06:39 +0900

    Fix Scala 3 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,8 +8,9 @@ import autoImport._
 
 object Dependencies {
   object shared {
-    val scalatest = Def.setting(("org.scalatest" %%% "scalatest" % "3.2.10" cross CrossVersion.for3Use2_13) % Test)
-    val scalatestHelper = Def.setting(("net.exoego" %%% "scalajs-test-helper-scalatest" % "0.2.0" cross CrossVersion.for3Use2_13) % Test)
+    val scalatest = Def.setting("org.scalatest" %%% "scalatest" % "3.2.10" % Test)
+    val scalatestHelper = Def.setting(("net.exoego" %%% "scalajs-test-helper-scalatest" % "0.2.0" cross CrossVersion.for3Use2_13) % Test
+      excludeAll (ExclusionRule("org.scalatest")))
     val compat = Def.setting("org.scala-lang.modules" %%% "scala-collection-compat" % "2.6.0")
   }
   object scalajs {}
